dd0a4b1c891c5891dd37b6f9cc8baaa86e909877
Aleksis <aleksis.vilums@gmail.com>
2020-09-10 17:06:19 -0600

    Chapter 8

---
+++ Chapter_8/DoubleJeopardy.java
@@ -0,0 +1,6 @@
+public class DoubleJeopardy{
+    public static void main( String[] args) {
+        double value = 32 * Math.pow(400, 1000);
+        System.out.println("A double: " + value);
+    }
+}
