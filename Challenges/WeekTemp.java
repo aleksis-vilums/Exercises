3ca2de84b027e5ed16f5fdabc2597814e86534cd
Aleksis <aleksis.vilums@gmail.com>
2020-09-19 16:49:12 -0600

    Challenges

--- Challenges/WeekTemp.java
+++ Challenges/WeekTemp.java
@@ -3,23 +3,25 @@ import java.util.Scanner;
 public class WeekTemp {
 
 	public static void main (String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		String[] daysOfWeek = {"Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"};
-		int temps[] = new int[7];
+        String[] daysOfWeek = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursady", "Friday", "Saturday"};
+        
+        int temps[] = new int[7];
+        
         int ans = 0;
+
+        Scanner scanner = new Scanner(System.in);
         
 		for (int i = 0; i < 7; i++){
-			System.out.print(daysOfWeek[i] + " temp: ");
+            System.out.print(daysOfWeek[i] + " temp: ");
 			temps[i] = Integer.parseInt(scanner.nextLine());
-
 			ans += temps[i];
 		}		
 
-		ans /= 7;
-
 		for (int i = 0; i < 7; i++){
 			System.out.print(daysOfWeek[i] + ": " + temps[i] + "F ");
-		}
+        }
+        
+        ans /= 7;
 
 		System.out.println("\b\nWeekly Average: " + ans + "F");
 
