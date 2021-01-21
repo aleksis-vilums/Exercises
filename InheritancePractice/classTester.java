7989d55e48dc30cd00fd09e7162f195921a4eae1
Aleksis <aleksis.vilums@gmail.com>
2021-01-21 11:25:43 -0700

    Number one done

---
+++ InheritancePractice/classTester.java
@@ -0,0 +1,16 @@
+public class classTester{
+    public static void main(String[] args) {
+        Class_2 test = new Class_2();
+        Class_2 test1 = new Class_2(5, 7, 2);
+
+        test.print();
+
+        test1.print();
+
+        test.set(3, 5, 4);
+
+        test.print();
+
+
+    }
+}
