53bab7e48d8d1ccddd4f9f47ade685dd8f5b762a
Aleksis <aleksis.vilums@gmail.com>
2021-02-18 11:47:39 -0700

    Exercies 1 and 2 done

--- Chapter_91/TriangleTester.java
+++ Chapter_91/TriangleTester.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class TriangleTester {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter a integer:");
-        int n = scan.nextInt();
+        int n = scanner.nextInt();
         if (n > 0){
             System.out.println(tri(n));
         } else {
