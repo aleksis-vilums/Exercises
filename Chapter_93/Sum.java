8a09c5747e86fce7a432f46cc2093a22bdb214d2
Aleksis Vilums <vilumsar@s.dcsdk12.org>
2021-02-19 08:56:21 -0700

    Exercise 3 done

--- Chapter_93/Sum.java
+++ Chapter_93/Sum.java
@@ -4,11 +4,23 @@ public class Sum {
         System.out.println(sum(testArray, 0)); //15
         System.out.println(sum(testArray, 1)); //12
         System.out.println(sum(testArray, 2)); //8
+        System.out.println(sum(testArray)); //15
+        
 
     }
 
 
     public static int sum(int[] array, int index){
+        if (array.length == index){
+            return 0;
+        } else {// index < array.length
+            return array[index] + sum(array, index + 1);
+        }
+    }
+
+    public static int sum(int[] array){
+        int index = 0;
+
         if (array.length == index){
             return 0;
         } else {// index < array.length
