b30849d50a26c5734811a6b27204c8f4bbfd403d
Aleksis Vilums <vilumsar@s.dcsdk12.org>
2021-02-19 08:52:42 -0700

    Exercise 2 done

---
+++ Chapter_93/Sum.java
@@ -0,0 +1,18 @@
+public class Sum {
+    public static void main(String[] args) {
+        int[] testArray = {3, 4, 5, 2, 1}; //15
+        System.out.println(sum(testArray, 0)); //15
+        System.out.println(sum(testArray, 1)); //12
+        System.out.println(sum(testArray, 2)); //8
+
+    }
+
+
+    public static int sum(int[] array, int index){
+        if (array.length == index){
+            return 0;
+        } else {// index < array.length
+            return array[index] + sum(array, index + 1);
+        }
+    }
+}
